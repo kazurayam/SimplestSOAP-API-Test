9cc6e6d784331c7029cba65298bb31163ae87551
kazurayam <kazuaki.urayama@gmail.com>
2021-08-11 11:31:01 +0900

    add CalculatorWithWorkaroundTest

--- Scripts/CalculatorWithWorkaroundTest/Script1628648915178.groovy
+++ Scripts/CalculatorWithWorkaroundTest/Script1628648915178.groovy
@@ -2,6 +2,11 @@ import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
 import com.kms.katalon.core.model.FailureHandling
 import com.kms.katalon.core.testobject.ResponseObject
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+
+// as suggested by @duylong at https://forum.katalon.com/t/simplest-soap-test-doent-work-in-ks-v8-0-x-property-http-javax-xml-xmlconstants-property-accessexternaldtd-is-not-recognized-due-to-accessexternaldtd/55765/5
+System.setProperty("javax.xml.parsers.DocumentBuilderFactory",
+	"com.sun.org.apache.xerces.internal.jaxp.DocumentBuilderFactoryImpl");
+
 ResponseObject response = WS.sendRequest(findTestObject('Calculator'), FailureHandling.CONTINUE_ON_FAILURE)
 WS.verifyResponseStatusCode(response, 200)
 String xmlString = response.getResponseBodyContent()
